762d01c1c8b23229c226b3fb6f5a85f7466d3012
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-07-19 00:13:28 +0900

    コメント追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+// main関数がrunApp関数を実行し、MyAppウィジェットを呼んでいる。
+// ここはパッケージのimport宣言の次に来る部分でwidgetではない。アプリの始まりになる部分。
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
+
+  // buildメソッドを利用してreturnしているwidgetをビルド
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
+      home: RandomWords(), //statefullメソッドを返すクラスを呼び出す
     );
   }
 }
 
+// StatefullWidgetクラス
+// Stateクラスをインスタンスとして生成し、widgetを呼び出す
+// Stateクラスを返すだけのシンプルな構造
 class RandomWords extends StatefulWidget {
   @override
+
+  // クラスのインスタンス化にnewは省略できる？
   _RandomWordsState createState() => _RandomWordsState();
+
+  /*
+  アロー関数を利用しないハージョン
+  _RandomWordsState createState() {
+    return ( _RandomWordsState() );
+  }
+  */
 }
 
+// Stateクラス
 class _RandomWordsState extends State<RandomWords> {
+  // WordPair型でリストを宣言
   final _suggestions = <WordPair>[];
+  // TextStyleクラスのコンストラクタ？
   final _biggerFont = TextStyle(fontSize: 18.0);
   @override
+
+  // buildメソッド
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -41,15 +62,15 @@ class _RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
-          return _buildRow(_suggestions[index]);
+          return _buildRow(_suggestions[index]); //
         });
   }
 
   Widget _buildRow(WordPair pair) {
     return ListTile(
       title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+        pair.asPascalCase, //パスカルケースの英単語
+        style: _biggerFont, // フォントサイズ18で出力
       ),
     );
   }
